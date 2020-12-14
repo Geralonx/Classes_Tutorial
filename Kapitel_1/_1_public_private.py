8fe11bd84592826e02eec77af5be57bfbf1a6c05
Geralonx <61078042+Geralonx@users.noreply.github.com>
2020-12-14 22:57:14 +0100

    Commentary/Namen fix

--- Kapitel_1/_1_public_private.py
+++ Kapitel_1/_1_public_private.py
@@ -1,3 +1,4 @@
+# Klassendeklaration mit Konstruktor
 class PC:
     def __init__(self, cpu, gpu, ram):
         self.cpu = cpu
@@ -7,18 +8,18 @@ class PC:
 
 # Instanziierung einer Klasse
 # Ich bevorzuge die Initialisierung mit den Keywords
-meine_pc_instanz = PC(cpu='Ryzen 7', gpu='RTX2070Super', ram='GSkill')
-
+pc_instanz = PC(cpu='Ryzen 7', gpu='RTX2070Super', ram='GSkill')
 
 # Zugriff auf normale _public_ Attribute
-print(meine_pc_instanz.prozessor)
-print(meine_pc_instanz.grafikkarte)
+print(pc_instanz.cpu)
+print(pc_instanz.gpu)
 
 # Zugriff auf ein _privates_ Attribut
-# print(meine_pc_instanz.__ram)
+# Auskommentiert, da es einen AttributeError schmeißt.
+# print(pc_instanz.__ram)
 
 # Zugriff auf das Instanz-Dictionary, um die Inhalte jener Instanz zu erhalten.
-print(meine_pc_instanz.__dict__)
+print(pc_instanz.__dict__)
 
-# Zugriff auf das eigentlich _priuvate_ Attribut.
-print(meine_pc_instanz.__ram)
+# Zugriff auf das eigentlich _private_ Attribut.
+print(pc_instanz._PC__ram)
